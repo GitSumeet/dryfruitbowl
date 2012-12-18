2bc3822cd793bfd6fe55cf1832cdd3362beaf225
Abhimanyu Sirohi <abhimanyu@netjoos.com>
2012-12-19 01:40:52 +0530

    precompiled assets for heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,15 +25,15 @@ $(document).ready(function() {
 		 I have been working on this project sporadically and here is the first release.\
 		 <br/><br/>\
 		 The project uses the following:<br/><br/>\
-		 Ruby 1.8.7<br/>Rails 3.2.7<br/>JQuery 1.8.3<br/>Metro UI CSS<br/>Messi Message Plugin. It was built using Aptana Studio 3 IDE on a Ubuntu 12.04 LTS Platform<br/>\
+		 Ruby 1.8.7<br/>Rails 3.2.7<br/>JQuery 1.8.3<br/>Metro UI CSS<br/>Messi Message Plugin.<br/>It was built using Aptana Studio 3 IDE on a Ubuntu 12.04 LTS Platform<br/>\
 		 <br/>\
 		 DryFruitBowl is hosted on free version of Heroku Cedar stack using just 1 web Dyno. \
 		 It uses the inbuilt PostgreSQL database addon on Heroku. Project source code is publicly accessible through \
 		 GitHub repository: https://github.com/a9s6/dryfruitbowl \
 		 <br/><br/>\
 		 The project was developed for learning Ruby on Rails. All images used in the project were downloaded from\
-		 the internet and are copyright of their respective owners. I do not own any of the images.</p>\
-		 I have plans to add a feedback form to the project. Until then please send in your feedback and suggestions to (abhimanyu@netjoos.com)<br/></div>', 
+		 the internet and are copyright of their respective owners. I do not own any of the images.\
+		 I have plans to add a feedback form to the project. Until then please send in your feedback and suggestions to (abhimanyu@netjoos.com)<br/></p></div>', 
 		{title: 'About', modal: true, width: 620}); 
 	});	
 })
